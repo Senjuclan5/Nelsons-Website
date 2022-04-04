7c2f89a4ee260c2befbca80b5a1e38c253fccd89
Senjuclan5 <88778429+Senjuclan5@users.noreply.github.com>
2022-04-04 14:03:13 -0400

    Testing adjustment to images directory

--- JSON/carouselImages.js
+++ JSON/carouselImages.js
@@ -2,37 +2,37 @@
 var carouselImages = [
     {
         "name" : "Girl I Love Portico",
-        "location": "./../images/girliloveportico.jpg",
+        "location": "./images/girliloveportico.jpg",
         "decription" : "Woman in Pink T-shirt with Print stating I Love Portico"
     },
     {
         "name" : "Blue Welding Shirt",
-        "location" : "../images/blueWeldingShirt.jpg",
+        "location" : "./images/blueWeldingShirt.jpg",
         "decription" : "Welder in Shirt We Printed his logo on"
     },
     {
         "name" : "cruise Shit",
-        "location" : "../images/cruise-shirt.jpg",
+        "location" : "./images/cruise-shirt.jpg",
         "decription" : "Cruise Shirt"
     },
     {
         "name" : "Plane Shirt",
-        "location" : "../images/plane-shirt.jpg",
+        "location" : "./images/plane-shirt.jpg",
         "decription" : "Plane Shirt"
     },
     {
         "name" : "Koozie",
-        "location" : "../images/koozie.jpg",
+        "location" : "./images/koozie.jpg",
         "decription" : "Koozie"
     },
     {
         "name" : "Hoodie",
-        "location" : "../images/jaket.jpeg",
+        "location" : "./images/jaket.jpeg",
         "decription" : "Hoodie"
     },
     {
         "name" : "Yellow Shirt",
-        "location" : "../images/yellowShirt.jpg",
+        "location" : "./images/yellowShirt.jpg",
         "decription" : "Yellow Shirt"
     }
 

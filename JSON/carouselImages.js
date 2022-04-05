b78e835991ff0dbc306f51c22f60193b4e4d567e
Senjuclan5 <88778429+Senjuclan5@users.noreply.github.com>
2022-04-05 14:34:36 -0400

    Update carouselImages.js

--- JSON/carouselImages.js
+++ JSON/carouselImages.js
@@ -7,32 +7,32 @@ var carouselImages = [
     },
     {
         "name" : "Blue Welding Shirt",
-        "location" : "../assets/images/blueWeldingShirt.JPG",
+        "location" : "./../assets/images/blueWeldingShirt.JPG",
         "decription" : "Welder in Shirt We Printed his logo on"
     },
     {
         "name" : "Cruise Shit",
-        "location" : "../assets/images/cruise-shirt.JPG",
+        "location" : "./../assets/images/cruise-shirt.JPG",
         "decription" : "Cruise Shirt"
     },
     {
         "name" : "Plane Shirt",
-        "location" : "../assets/images/plane-shirt.JPG",
+        "location" : "./../assets/images/plane-shirt.JPG",
         "decription" : "Plane Shirt"
     },
     {
         "name" : "Koozie",
-        "location" : "../assets/images/koozie.JPG",
+        "location" : "./../assets/images/koozie.JPG",
         "decription" : "Koozie"
     },
     {
         "name" : "Hoodie",
-        "location" : "../assets/images/jaket.JPEG",
+        "location" : "./../assets/images/jaket.JPEG",
         "decription" : "Hoodie"
     },
     {
         "name" : "Yellow Shirt",
-        "location" : "../assets/images/yellowShirt.JPG",
+        "location" : "./../assets/images/yellowShirt.JPG",
         "decription" : "Yellow Shirt"
     }
 

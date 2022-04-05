ef2fc08e680d9646e5c50979016a531371add10d
Roman <RomanD2@Students.Manateeschools.net>
2022-04-05 14:23:46 -0400

    fresh reupload with multiple changes

---
+++ JSON/testimonialList.js
@@ -0,0 +1,30 @@
+/*var testimonialList = [
+    {
+        "name" : "Jhon Doe",
+        "date of service": "4/22/2022",
+        "service rating" : "",
+        "testimonial" : "thelkjm  ohdopa h oisdjflas foadhf lkdsjfadkfalskjdffapofjaj",
+
+    },
+    {
+        "name" : "Jane Doe",
+        "date of service": "5/23/2020",
+        "service rating" : "",
+        "testimonial" : "dfj;lasdkjgf klajdffln ; ajdfgofoyhtk nbalksdypoja;l;mnczn ;oaihfjha",
+        
+    },
+    {
+        "name" : "Mac  Miller",
+        "date of service": "6/9/2019",
+        "service rating" : "",
+        "testimonial" : "jflkaj;lkdq aoidh fkl [parjf nashfpwoerhi",
+    },
+    {
+        "name" : "",
+        "date of service": "",
+        "service rating" : "",
+        "testimonial" : "",
+    },
+]
+
+export default testimonialList*/
---
+++ jsMod/testimonial.js
@@ -0,0 +1,20 @@
+/*import testimonialList from '../JSON/testimonialList.js';
+
+function createTestimonialItems(){
+
+    var TestimonialList = testimonialList.map((TestimonialItem)=>{
+        var testimonialElement = `
+        <ul class = "testimonialCarouselTestimonial">
+            <li class = "testimonialCarouselItem" > `+TestimonialItem.testimonial +`</li>
+            <li class = "testimonialCarouselName">`+ TestimonialItem.name +`</li>
+        </ul>
+        `
+        return testimonialElement
+
+    })
+
+    return TestimonialList.join("")
+}
+
+
+export default createTestimonialItems;*/
